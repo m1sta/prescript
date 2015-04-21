b1de408907e6cd768ce14638dba0270d4c690cc5
Jonathon <git.jon@thon.id.au>
2015-04-21 13:50:30 +0100

    .

--- test/io/Result chaining.pre.js
+++ test/io/Result chaining.pre.js
@@ -1,1 +1,3 @@
-score = await db.get(input) >> JSON.parse(*) >> parseInt(*.value) / 100 
+score = await db.get(input) >> JSON.parse(*) >> parseInt(*.value) / 100 //use of asterix in identifier position in expression after >> operator
+bitshift = 9 >> 3 // no use of asterix
+bitshift = 152 >> 2 * 3 //use of asterix in operator position, not in identifier position.
